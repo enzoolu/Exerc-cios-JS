5976985408655dec6180bb4bd557a5188a451887
enzo goulart <enzoo.lu@gmail.com>
2022-11-28 20:49:39 -0300

    exercicio29

--- Ex29/script.js
+++ Ex29/script.js
@@ -9,14 +9,16 @@ function sequencia() {
     let num2 = 1;
     let resultado = 0;
 
-    for(let i = 0; i < 15; i++) {
+    for(let i = 1; i < 16; i++) {
+        resultado = num1;
+
+        const element = document.createElement("h2"); 
+        const inserir = document.createTextNode('Termo' + i + ': ' + resultado);
+
+        resultado = num2;
         resultado = num1 + num2;
         num1 = num2;
         num2 = resultado;
-
-
-        const element = document.createElement("h2"); 
-        const inserir = document.createTextNode(resultado);
 
         element.appendChild(inserir);
         const body = document.body;
